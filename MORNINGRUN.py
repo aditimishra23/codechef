784d3f1845cdbeea01a628a068e5d38a8eaba619
aditi <aditi@192.168.1.5>
2024-05-29 22:25:32 +0530

    First commit

---
+++ MORNINGRUN.py
@@ -0,0 +1,9 @@
+# Codechef morning run
+# Aditi Mishra
+# Diff: 306
+X, Y = map(int, input().split())
+p = 2 * (X + Y)
+if p >= 1000:
+    print("YES")
+else:
+    print("NO")
---
+++ RCBCSK.py
@@ -0,0 +1,8 @@
+# Codechef morning run
+# Aditi Mishra
+# Diff: 282
+x, y = map(int, input().split())
+if (x - y) >= 18:
+    print("RCB qualified.")
+else:
+    print("CSK qualified.")
